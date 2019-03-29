fc1f6ae8e5e7bd6e5ef3c5fb19277bab7e76a74d
kimscott <sanaloveyou@uengine.org>
2019-03-29 14:03:58 +0900

    토큰 사용안하도록 일단 변경

--- service-gateway/src/main/java/com/example/template/Application.java
+++ service-gateway/src/main/java/com/example/template/Application.java
@@ -3,7 +3,6 @@ package com.example.template;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 public class Application {
--- service-gateway/src/main/java/com/example/template/JwkSetEndpointConfiguration.java
+++ service-gateway/src/main/java/com/example/template/JwkSetEndpointConfiguration.java
@@ -1,31 +1,31 @@
-package com.example.template;
-
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.security.KeyPair;
-import java.security.Principal;
-import java.security.interfaces.RSAPublicKey;
-import java.util.Map;
-
-//@FrameworkEndpoint
-@RestController
-public class JwkSetEndpointConfiguration {
-    KeyPair keyPair;
-
-    public JwkSetEndpointConfiguration(KeyPair keyPair) {
-        this.keyPair = keyPair;
-    }
-
-    @GetMapping("/.well-known/jwks.json")
-    @ResponseBody
-    public Map<String, Object> getKey(Principal principal) {
-        RSAPublicKey publicKey = (RSAPublicKey) this.keyPair.getPublic();
-        RSAKey key = new RSAKey.Builder(publicKey).build();
-        return new JWKSet(key).toJSONObject();
-    }
-}
+//package com.example.template;
+//
+//import com.nimbusds.jose.jwk.JWKSet;
+//import com.nimbusds.jose.jwk.RSAKey;
+//import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.security.KeyPair;
+//import java.security.Principal;
+//import java.security.interfaces.RSAPublicKey;
+//import java.util.Map;
+//
+////@FrameworkEndpoint
+//@RestController
+//public class JwkSetEndpointConfiguration {
+//    KeyPair keyPair;
+//
+//    public JwkSetEndpointConfiguration(KeyPair keyPair) {
+//        this.keyPair = keyPair;
+//    }
+//
+//    @GetMapping("/.well-known/jwks.json")
+//    @ResponseBody
+//    public Map<String, Object> getKey(Principal principal) {
+//        RSAPublicKey publicKey = (RSAPublicKey) this.keyPair.getPublic();
+//        RSAKey key = new RSAKey.Builder(publicKey).build();
+//        return new JWKSet(key).toJSONObject();
+//    }
+//}
