69a8b756e34d20dc48f2b8fac3e102bef3590d98
kimscott <sanaloveyou@uengine.org>
2019-04-01 09:47:39 +0900

    토큰 사용변경

--- service-gateway/src/main/java/com/example/template/ResourceServerConfiguration.java
+++ service-gateway/src/main/java/com/example/template/ResourceServerConfiguration.java
@@ -1,56 +1,43 @@
-//package com.example.template;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.ClassPathResource;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-//import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//
-//import java.security.KeyPair;
-//
-//@Configuration
-//@EnableWebFluxSecurity
-//public class ResourceServerConfiguration {
-//
-//    @Bean
-//    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
-//        http
-//                .authorizeExchange()
-//                .pathMatchers("/.well-known/jwks.json")
-//                .permitAll();
-//
-//        http
-//                .authorizeExchange()
-//                .pathMatchers("/oauth/**","/login/**").permitAll()
-//                .anyExchange().authenticated()
-//                .and()
-//                .oauth2ResourceServer()
-//                .jwt()
-//                ;
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public KeyPair makeKeyPair(){
-//        KeyPair keyPair = new KeyStoreKeyFactory(
-//                new ClassPathResource("server.jks"), "qweqwe".toCharArray())
-//                .getKeyPair("uengine", "qweqwe".toCharArray());
-//        return keyPair;
-//    }
-//
-////    @Override
-////    public void configure(final HttpSecurity http) throws Exception {
-////        http.authorizeRequests()
-////                .antMatchers("/oauth/**","/login/**")
-////                .permitAll()
-////                .antMatchers("/**")
-////                .authenticated();
-////    }
-//
-//}
+package com.example.template;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.security.KeyPair;
+
+@Configuration
+@EnableWebFluxSecurity
+public class ResourceServerConfiguration {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+
+        http
+                .cors().and()
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/oauth/**","/login/**","/.well-known/jwks.json").permitAll()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                ;
+
+        return http.build();
+    }
+
+    @Bean
+    public KeyPair makeKeyPair(){
+        KeyPair keyPair = new KeyStoreKeyFactory(
+                new ClassPathResource("server.jks"), "qweqwe".toCharArray())
+                .getKeyPair("uengine", "qweqwe".toCharArray());
+        return keyPair;
+    }
+
+
+}
